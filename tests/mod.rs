52dc97d50d85fd70b6f54e725e345780eca71432
Rahul Kumar <rahulkumar@berkeley.edu>
2022-03-14 11:21:18 -0700

    add bitcell pex test

--- tests/mod.rs
+++ tests/mod.rs
@@ -5,7 +5,7 @@ use crate::verification::pex::{Pex, PexInput, PexOpts};
 use super::MagicPex;
 
 #[test]
-fn test_pex_sky130() -> Result<(), Box<dyn std::error::Error>> {
+fn test_pex_sky130_nand2() -> Result<(), Box<dyn std::error::Error>> {
     let work_dir: PathBuf = "/tmp/sram22/tests/pex/nand2".into();
     let base = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let layout =
@@ -13,6 +13,22 @@ fn test_pex_sky130() -> Result<(), Box<dyn std::error::Error>> {
     let _output = MagicPex::pex(PexInput {
         layout,
         layout_cell: "nand2_dec_auto".to_string(),
+        work_dir,
+        tech: "sky130A".to_string(),
+        opts: PexOpts {},
+    })?;
+    Ok(())
+}
+
+#[test]
+fn test_pex_sky130_bitcell() -> Result<(), Box<dyn std::error::Error>> {
+    let work_dir: PathBuf = "/tmp/sram22/tests/pex/bitcell".into();
+    let base = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let layout =
+        base.join("src/verification/plugins/magic_pex/tests/data/bitcell/sram_sp_cell.mag");
+    let _output = MagicPex::pex(PexInput {
+        layout,
+        layout_cell: "sram_sp_cell".to_string(),
         work_dir,
         tech: "sky130A".to_string(),
         opts: PexOpts {},
